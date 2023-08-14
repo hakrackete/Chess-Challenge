bec4ae6b8dbb8127b0a9c793821fef70c8cf78bb
Marvin <marvin@hofmannwelt.de>
2023-08-14 13:07:16 +0200

    lol

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,12 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        Console.WriteLine("hi");
         return moves[0];
     }
 }
