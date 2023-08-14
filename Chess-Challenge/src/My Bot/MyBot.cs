47dd72e4d698233874c3872405d253979080f2b7
Marvin <marvin@hofmannwelt.de>
2023-08-14 16:23:46 +0200

    beste variant bisher

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,5 +1,6 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
@@ -11,61 +12,68 @@ public class MyBot : IChessBot
         int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
 
         Move[] moves = board.GetLegalMoves();
+        Dictionary<Move,double> moveEval = new Dictionary<Move, double>();
+
         Random rng = new();
         Move moveToReturn = moves[rng.Next(moves.Length)];
-        int highestCapture = 0;
 
         foreach (Move move in moves)
         {
             if(MoveIsCheckmate(board,move)){
-                moveToReturn = move;
-                break;
+                return move;
             }
-
-        // wenn keine, dann durchsuchen, welche Moves ein pieve capturen, dabei den höchsten pieceValue beachten
-            if(pieceValues[(int)move.CapturePieceType] > highestCapture ){
-                highestCapture = pieceValues[(int)move.CapturePieceType];
-                Console.WriteLine(highestCapture);
+            double bestMoveScore = 0;
+            double score = evaluateMove(board,move);
+            // do something with entry.Value or entry.Key
+            if(score > bestMoveScore){
+                bestMoveScore = score;
                 moveToReturn = move;
             }
-
         }
+
+
         // wenn keine captures, dann versuchen bauern zu advancen?
         int lowestDistance = 7;
-        if(highestCapture == 0){
-            foreach(Move pawnmove in moves){
-                Piece myPiece = board.GetPiece(pawnmove.StartSquare);
-                if(myPiece.IsPawn == false){
-                    continue;
-                }
-                if (board.SquareIsAttackedByOpponent(pawnmove.TargetSquare)){
-                    if(MoveIsGuarded(board,pawnmove) == false){
-                        continue;
-                    }
+        // if(highestCapture == 0){
+        //     foreach(Move pawnmove in moves){
+        //         Piece myPiece = board.GetPiece(pawnmove.StartSquare);
+        //         if(myPiece.IsPawn == false){
+        //             continue;
+        //         }
+        //         if (board.SquareIsAttackedByOpponent(pawnmove.TargetSquare)){
+        //             if(MoveIsGuarded(board,pawnmove) == false){
+        //                 continue;
+        //             }
 
-                }
-                if(pawnmove.IsPromotion){
-                    moveToReturn = pawnmove;
-                    break;
-                }
-                Square start = pawnmove.StartSquare;
-                Square target = pawnmove.TargetSquare;
-                int distance = 7 - target.Rank;
-                if(target.Rank - start.Rank < 0){
-                    distance = target.Rank;
-                }
-                if(distance < lowestDistance){
-                    lowestDistance = distance;
-                    moveToReturn = pawnmove;
-                }
-            }
-        }
+        //         }
+        //         if(pawnmove.IsPromotion){
+        //             moveToReturn = pawnmove;
+        //             break;
+        //         }
+        //         Square start = pawnmove.StartSquare;
+        //         Square target = pawnmove.TargetSquare;
+        //         int distance = 7 - target.Rank;
+        //         if(target.Rank - start.Rank < 0){
+        //             distance = target.Rank;
+        //         }
+        //         if(distance < lowestDistance){
+        //             lowestDistance = distance;
+        //             moveToReturn = pawnmove;
+        //         }
+        //     }
+        // }
         Console.WriteLine(MoveIsGuarded(board,moveToReturn));
         return moveToReturn;
     }
     public bool MoveIsCheckmate(Board board, Move move){
         board.MakeMove(move);
-        bool isCheck = board.IsInCheckmate();
+        bool isCheckMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isCheckMate;
+    }
+    public bool MoveIsCheck(Board board, Move move){
+        board.MakeMove(move);
+        bool isCheck = board.IsInCheck();
         board.UndoMove(move);
         return isCheck;
     }
@@ -76,7 +84,27 @@ public class MyBot : IChessBot
         bool isGuarded = board.SquareIsAttackedByOpponent(target);
         board.UndoMove(move);
         return isGuarded;
-        
+    }
+    public double evaluateMove(Board board, Move move){
+        double score = 0;
+        // plus für captures
+        score += (int)move.CapturePieceType;
 
+        // minus wenn angegriffen werden kann
+        if (board.SquareIsAttackedByOpponent(move.TargetSquare)){
+            if(MoveIsGuarded(board,move) == true){
+                score -= 0;
+            }
+            else{
+                score -= (int)move.MovePieceType;
+            }
+        }
+        if(MoveIsCheck(board,move) && MoveIsGuarded(board,move)){
+            score += 500;
+        }
+        if(move.IsPromotion){
+            score += (int)move.PromotionPieceType;
+        }
+        return score;
     }
 }
